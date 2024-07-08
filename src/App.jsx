93332c06b1fc4f63530565db84354e77c19c66ec
Michael Galo <michael@michaelthinks.co>
2024-07-08 10:13:29 -0500

    ERD

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
-}
+  const [count, setCount] = useState(0); // [stateVariable, setterFunction]
+
+  const handleBtnClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <h1>Hello!</h1>
+      <div>This is amazing!</div>
+      <button onClick={handleBtnClick} className="btn-secondary">
+        Click Me!
+      </button>
+      <div>Count: {count}</div>
+    </>
+  );
+};

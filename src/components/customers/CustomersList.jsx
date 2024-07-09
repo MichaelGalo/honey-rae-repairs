2a42dd4b73bcb352280ab33bdc46af83c8f75e23
Michael Galo <michael@michaelthinks.co>
2024-07-09 14:45:47 -0500

    customer list component added

--- src/components/customers/CustomersList.jsx
+++ src/components/customers/CustomersList.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { getNonStaffUsers } from "../../services/userService";
+import { Users } from "../users/Users";
+import "./Customers.css";
 
 export const CustomersList = () => {
   const [customers, setCustomers] = useState([]);
@@ -13,18 +15,7 @@ export const CustomersList = () => {
   return (
     <div className="customers">
       {customers.map((customerObj) => {
-        return (
-          <div key={customerObj.id} className="customer">
-            <div>
-              <div>Name</div>
-              <div>{customerObj.fullName}</div>
-            </div>
-            <div>
-              <div>Email</div>
-              <div>{customerObj.email}</div>
-            </div>
-          </div>
-        );
+        return <Users key={customerObj.id} user={customerObj} />;
       })}
     </div>
   );

46573a6d0c037853e959992229e545688dc8113e
Michael Galo <michael@michaelthinks.co>
2024-07-16 13:53:37 -0500

    added customer tickets component

--- src/components/tickets/TicketList.jsx
+++ src/components/tickets/TicketList.jsx
@@ -12,13 +12,20 @@ export const TicketList = ({ currentUser }) => {
 
   const getAndSetTickets = () => {
     getAllTickets().then((ticketsArray) => {
-      setAllTickets(ticketsArray);
+      if (currentUser.isStaff) {
+        setAllTickets(ticketsArray);
+      } else {
+        const customerTickets = ticketsArray.filter(
+          (ticket) => ticket.userId === currentUser.id
+        );
+        setAllTickets(customerTickets);
+      }
     });
   };
 
   useEffect(() => {
     getAndSetTickets();
-  }, []); // ONLY runs on initial render of component
+  }, [currentUser]);
 
   useEffect(() => {
     // filter tickets
